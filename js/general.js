f4b0d2968f01f9930b9d1940932afc23eb939249
Zolice <vividzhenlong@gmail.com>
2023-02-16 19:25:46 +0800

    Refactored Header and Social Media to external HTML files
    Also reorganized index.html. 
    Removed Categories as it is no longer required.

--- js/general.js
+++ js/general.js
@@ -1,33 +1,39 @@
 var header
-var headerLink
-var headerCategory
+var darkHeader
+var lightHeader
+
+var sidebar
 
 window.onload = () => {
-    header = document.getElementById('header')
-    headerLink = document.getElementById('header-link')
-    headerCategory = document.getElementById('header-category')
+    addHeader()
+    addSidebar()
+}
 
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 80) {
-            header.style.backgroundImage = "none"
-            header.style.backgroundColor = "aliceblue"
-            header.classList.remove("text-dark")
+function setHeaderStyle() {
+    if (window.scrollY > 432) {
+        header.classList.add("header-light")
+        darkHeader.style.display = "none"
+        lightHeader.style.display = "flex"
+    }
+    else {
+        header.classList.remove("header-light")
+        darkHeader.style.display = "flex"
+        lightHeader.style.display = "none"
+    }
+}
 
-            // headerLink.classList.remove("link-text-dark")
+function addHeader() {
+    header = document.getElementById('header-div')
 
-            // for (let i = 0; i < headerCategory.children.length; i++) {
-            //     headerCategory.children[i].classList.remove("text-dark")
-            // }
-        } else {
-            header.style.backgroundImage = "linear-gradient(to bottom, white, transparent)";
-            header.style.backgroundColor = "transparent"
-            header.classList.add("text-dark")
+    $('#header-div').load('./assets/elements/header.html', function () {
+        darkHeader = document.getElementById('header-dark')
+        lightHeader = document.getElementById('header-light')
 
-            // headerLink.classList.add("link-text-dark")
-
-            // for (let i = 0; i < headerCategory.children.length; i++) {
-            //     headerCategory.children[i].classList.add("text-dark")
-            // }
-        }
+        setHeaderStyle()
+        window.addEventListener("scroll", setHeaderStyle)
     });
+}
+
+function addSidebar() {
+    $('#sidebar').load('./assets/elements/social-media-feed.html');
 }
